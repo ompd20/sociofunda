6dc754ef78b51ccc84708ee7d64d1b864f08ebeb
ompd <91319039+ompd@users.noreply.github.com>
2024-06-19 15:04:33 +0530

    Initial commit on new branch

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,12 +5,4 @@ namespace Config;
 use CodeIgniter\Router\RouteCollection;
 use CodeIgniter\Router\RouteGroup;
 
-$routes = Services::routes();
-
-$routes->group('', ['namespace' => 'App\Controllers'], function (RouteCollection $routes) {
-    $routes->get('/', 'Home::index');
-});
-
-$routes->group('api', ['namespace' => 'App\Controllers', 'filter' => 'jwt'], function (RouteGroup $routes) {
-    $routes->get('home', 'Home::index');
-});
+$routes->get('/', 'Home::index');
